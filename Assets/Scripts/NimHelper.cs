1a7f4559cb8ceda551b6ba309fc86217fe73e2be
Martin Kolev <martinkolevbg93@gmail.com>
2019-12-19 13:11:49 +0200

    Added Random coin generator.

---
+++ Assets/Scripts/NimHelper.cs
@@ -0,0 +1,15 @@
+﻿public class NimHelper
+{
+    public static int CalculateCoinsToRemove(int coinsLeft)
+    {
+        int coinsToTake = (coinsLeft -1) % 4;
+
+        if(coinsToTake  == 0)
+        {
+            return 1;
+        }   
+        else{
+            return coinsToTake;
+        }
+    }
+}
---
+++ Assets/Scripts/RandomGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenerator : MonoBehaviour
+{
+    public GameObject coin;
+
+    System.Random rand;
+
+    public int minCoins = 11;
+    public int maxCoins = 39;
+
+    void Start()
+    {
+        rand = new System.Random();
+
+        float xPos;
+        float yPos;
+        float zPos;
+
+        int numberOfCoins = rand.Next (maxCoins - minCoins) + minCoins;
+      
+        Debug.Log ("Instantiating " + numberOfCoins + " coins");
+
+        for(int i= 0; i < numberOfCoins; i++)
+        {
+            xPos = rand.Next(100) / 100f;
+            yPos = rand.Next(5) + 2f;
+            zPos = rand.Next(100) / 100f;
+            
+            Instantiate (coin, new Vector3(0,1,0), Random.rotation);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
